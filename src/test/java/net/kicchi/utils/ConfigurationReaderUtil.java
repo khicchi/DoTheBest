b5c1e22e9bc2d6ca42785a5b63bdfb0e00aac58b
ececi <huseyinkiceci@gmail.com>
2022-02-28 12:18:06 +0300

    utils added pom updated

---
+++ src/test/java/net/kicchi/utils/ConfigurationReaderUtil.java
@@ -0,0 +1,26 @@
+package net.kicchi.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import net.kicchi.config.ApplicationConfig;
+
+import java.io.File;
+
+/**
+ * reads the properties file configuration.yml
+ */
+public class ConfigurationReaderUtil {
+
+    public static ApplicationConfig getConfiguration(){
+        try{
+            File file = new File("src/test/resources/configuration.yml");
+            ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+            ApplicationConfig applicationConfig = objectMapper.readValue(file, ApplicationConfig.class);
+            return applicationConfig;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
